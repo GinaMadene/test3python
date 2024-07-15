b750876f005c56346cb6d035fe9adf2e27f8ce10
GinaMadene <ginamadene@gmail.com>
2024-07-15 11:45:34 +0000

    init

--- test3.py
+++ test3.py
@@ -1,46 +1,45 @@
 import pymysql
 
-# Connexion à la base de données BaseDeDonnees1
+# Connexion à la nouvelle BaseDeDonnees1 (pour récupérer les données)
 conn1 = pymysql.connect(
     host="localhost",
     user="root",
     password="",
-    database="BaseDeDonnees1"
+    database="NouvelleBaseDeDonnees1"
 )
 
-# Connexion à la base de données BaseDeDonnees2
+# Connexion à la nouvelle BaseDeDonnees2 (pour insérer les données)
 conn2 = pymysql.connect(
     host="localhost",
     user="root",
     password="",
-    database="BaseDeDonnees2"
+    database="NouvelleBaseDeDonnees2"
 )
 
 cursor1 = conn1.cursor()
 cursor2 = conn2.cursor()
 
 try:
-    # Récupérer les données de TableA de BaseDeDonnees1
-    cursor1.execute("SELECT nom FROM TableA")
+    # Récupérer les données de NouvelleTableA de NouvelleBaseDeDonnees1
+    cursor1.execute("SELECT nom FROM NouvelleTableA")
     rows_tableA = cursor1.fetchall()
 
-    # Récupérer les données de TableB de BaseDeDonnees1
-    cursor1.execute("SELECT description FROM TableB")
+    # Récupérer les données de NouvelleTableB de NouvelleBaseDeDonnees1
+    cursor1.execute("SELECT description FROM NouvelleTableB")
     rows_tableB = cursor1.fetchall()
 
-    # Associer les données de TableA et TableB et insérer dans TableC de BaseDeDonnees2
+    # Insertion des données récupérées dans NouvelleTableC de NouvelleBaseDeDonnees2
     for nom, description in zip(rows_tableA, rows_tableB):
-        cursor2.execute("INSERT INTO TableC (nom, description) VALUES (%s, %s)", (nom[0], description[0]))
+        cursor2.execute("INSERT INTO NouvelleTableC (nom, description) VALUES (%s, %s)", (nom[0], description[0]))
 
-    # Valider les modifications dans la base de données BaseDeDonnees2
     conn2.commit()
-    print("Insertion réussie dans la table TableC de BaseDeDonnees2")
+    print("Insertion de données réussie dans NouvelleTableC de NouvelleBaseDeDonnees2")
 
-    # Récupérer et afficher les données insérées dans TableC de BaseDeDonnees2
-    cursor2.execute("SELECT * FROM TableC")
+    # Afficher les données insérées dans NouvelleTableC pour vérification
+    cursor2.execute("SELECT * FROM NouvelleTableC")
     rows_tableC = cursor2.fetchall()
 
-    print("Données insérées dans TableC de BaseDeDonnees2:")
+    print("Données insérées dans NouvelleTableC de NouvelleBaseDeDonnees2:")
     for row in rows_tableC:
         print(row)
 
@@ -48,7 +47,7 @@ except pymysql.MySQLError as err:
     print(f"Erreur MySQL : {err}")
 
 finally:
-    # Fermer les connexions
+    # Fermeture des connexions
     cursor1.close()
     conn1.close()
     cursor2.close()
